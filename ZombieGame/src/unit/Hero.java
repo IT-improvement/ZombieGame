811726a153ee67d8b6590fb7bb91b01a2a129f9f
seungjae <juaje03@naver.com>
2024-03-26 18:15:33 +0900

    Create: unit package/Boss, Hero, Zombie class

---
+++ ZombieGame/src/unit/Hero.java
@@ -0,0 +1,18 @@
+package unit;
+
+import controller.Unit;
+import model.*;
+
+public class Hero extends Unit implements Damage, Heal, Move {
+
+	@Override
+	public void heal(int n) {
+
+	}
+
+	@Override
+	public void damage(int n) {
+
+	}
+
+}
---
+++ ZombieGame/src/unit/Zombie.java
@@ -0,0 +1,13 @@
+package unit;
+
+import controller.Unit;
+import model.Damage;
+
+public class Zombie extends Unit implements Damage{
+
+	@Override
+	public void damage(int n) {
+		
+	}
+
+}
