215b4c7860dd0603fa89241393161d49bbf763e7
seungjae <juaje03@naver.com>
2024-03-26 18:23:09 +0900

    Add: variable hp(int), power(int) and getter, setter Method in Unit class

--- ZombieGame/src/controller/Unit.java
+++ ZombieGame/src/controller/Unit.java
@@ -1,4 +1,23 @@
 package controller;
 
 public abstract class Unit {
+	private int hp;
+	private int power;
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public int getPower() {
+		return power;
+	}
+
+	public void setPower(int power) {
+		this.power = power;
+	}
+
 }
